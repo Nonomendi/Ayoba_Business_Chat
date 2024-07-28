ad66c997f6de74fd5c482a95ad1a756ffb32da3d
Thulani Tyaphile <tj.tyaphile@gmail.com>
2024-07-28 05:02:30 +0200

    Message deletion complete

--- app/chat/message.py
+++ app/chat/message.py
@@ -4,6 +4,7 @@ import json
 username = "b54ba9dca84def801bf0fe59602c82ef75d7f4ee"
 password = "zIAq9U4UQ04Md5bT7hSiyjWbrcIq6hO"
 
+
 def login():
     body = {
         "username": "b54ba9dca84def801bf0fe59602c82ef75d7f4ee",
@@ -14,10 +15,12 @@ def login():
 
 
 # To-Do, save the mesages to the database
+# If messageId already in table, update column
 def get_messages(access_token):
     return json.loads(requests.get("https://api.ayoba.me/v1/business/message", headers={'Authorization': "Bearer " + access_token}).text)
 
 
+# Send message to numbers/msisdns
 def send_message(access_token:str, message:str, *msisdns):
     body = {
         "msisdns": list(msisdns),
@@ -27,9 +30,35 @@ def send_message(access_token:str, message:str, *msisdns):
         }
     }
 
+    results = json.loads(requests.post("https://api.ayoba.me/v1/business/message", json=body, headers={'Authorization': "Bearer " + access_token}).text)
+
+    for msg in results:
+        print("Update DB with 'msisdn':" + msg["msisdn"] + " 'message_id': " + msg["messageId"] + " 'message': " + message)
+
+    return results
+
+
+# Delete message
+def delete_message(access_token:str, message_id:str, *msisdns):
+    body = {
+        "msisdns": list(msisdns),
+        "message": {
+            "correlationId":message_id,
+            "type": "delete"
+        }
+    }
+
     return json.loads(requests.post("https://api.ayoba.me/v1/business/message", json=body, headers={'Authorization': "Bearer " + access_token}).text)
 
 
+# Needs assess to DB
+def message_reaction(messageID:str, reaction:str):
+    reactions = {
+        "love": "❤️",
+        "thumbs-up": "👍",
+        "thumbs-down": "👎",
+        "laugh": "😂",
+        "shock": "🫨",
+        "sad": "😥"
+    }
 
-access_token = login()
-print(send_message(access_token, "Mock message", "+27838430298"))
